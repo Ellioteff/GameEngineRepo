2b60857338e1913dad0581bc9cffe7f93867eff1
Ellioteff <elliotfaltstrom@hotmail.com>
2015-01-29 18:07:01 +0100

    afgasg

--- src/GameEngine/DynamicSprite.java
+++ src/GameEngine/DynamicSprite.java
@@ -1,10 +1,19 @@
 package GameEngine;
 
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Area;
+import java.awt.image.BufferedImage;
+
 public class DynamicSprite extends Sprite {
 
-	
+	public DynamicSprite(int width, int height, int x, int y, BufferedImage image) {
+
+		super(width,height,x,y,image);
+
+	}
 	public void move() {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/GameEngine/Sprite.java
+++ src/GameEngine/Sprite.java
@@ -1,5 +1,6 @@
 package GameEngine;
 
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Area;
@@ -23,7 +24,14 @@ public abstract class Sprite extends JComponent {
 		System.out.println("collision with " + s.toString());
 	}
 
-	public Sprite(int width, int height, int x, int y, BufferedImage image) {
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(spriteImage, 0, 0, this);
+
+	}
+
+	protected Sprite(int width, int height, int x, int y, BufferedImage image) {
 
 		area = new Area(new Rectangle(width, height));
 		xPos = x;
@@ -32,10 +40,10 @@ public abstract class Sprite extends JComponent {
 		spriteImageG2D = (Graphics2D) this.spriteImage.getGraphics();
 
 	}
-	
+
 	public Graphics2D getSpriteG2D() {
-        return spriteImageG2D;
-    }
+		return spriteImageG2D;
+	}
 
 	public abstract void move();
 
