7e88b31c5f53a4134dcfaa2bbc1f68b6218707c8
Ellioteff <elliotfaltstrom@hotmail.com>
2015-01-29 17:37:34 +0100

    sprites stuff

--- src/GameEngine/Sprite.java
+++ src/GameEngine/Sprite.java
@@ -1,19 +1,41 @@
 package GameEngine;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.*;
 
 public abstract class Sprite {
-	Area area;
-	
-	public Sprite(){
-		
-	}
-	
-	public void actOnCollision(Sprite s){
-		System.out.println("collision with "+s.toString());
-	}
-	
-	public abstract void move();
-	
-}
+	private BufferedImage spriteImage;
+	private Graphics2D spriteImageG2D;
+	private Area area;
+	private int xPos;
+	private int yPos;
 
+	public Sprite() {
+
+	}
+
+	public void actOnCollision(Sprite s) {
+		System.out.println("collision with " + s.toString());
+	}
+
+	public Sprite(int width, int height, int x, int y, BufferedImage image) {
+
+		area = new Area(new Rectangle(width, height));
+		xPos = x;
+		yPos = y;
+		spriteImage = image;
+		spriteImageG2D = (Graphics2D) this.spriteImage.getGraphics();
+
+	}
+	
+	public Graphics2D getSpriteG2D() {
+        return spriteImageG2D;
+    }
+
+	public abstract void move();
+
+}
