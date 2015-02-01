145361beac495386629d62c6f1a9337a9e934c45
Ellioteff <elliotfaltstrom@hotmail.com>
2015-02-01 20:45:54 +0100

    studsande silvia med transformers ljud!

--- src/TheGame/SilviaSprite.java
+++ src/TheGame/SilviaSprite.java
@@ -6,18 +6,24 @@ import GameEngine.SoundObject;
 public class SilviaSprite extends DynamicSprite {
 	SoundObject sound = new SoundObject("assets/sounds/Transformer Stingers 01 A.wav");
 	int soundCounter = 0;
+	int incrementX = 1;
+	int incrementY = 1;
 
 	public SilviaSprite() {
-		super(200, 200, 200, 200, "assets/sprites/Silvia.png");
+		super(50, 50, 0, 0, "assets/sprites/Silvia.png");
 	}
 
 	@Override
 	public void move() {
-		xPos+=1;
+		xPos += 1 * incrementX;
+		yPos += 1 * incrementY;
+		if(yPos<1 || yPos >= 829)
+			incrementY*=(-1);
+		if(xPos<1 || xPos >= 1190)
+			incrementX*=(-1);
 		soundCounter++;
-		if(soundCounter == 200 || soundCounter == 500)
+		if (soundCounter == 1)
 			sound.playSound();
-		
 
 	}
 
