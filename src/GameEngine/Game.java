3777efead41cf975b153fbd44d9cfd62184d8567
Rasmus <svampen.elite@gmail.com>
2015-02-03 01:16:03 +0100

    mini changes

--- src/GameEngine/Game.java
+++ src/GameEngine/Game.java
@@ -1,7 +1,6 @@
 package GameEngine;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -17,6 +16,10 @@ public class Game implements Serializable {
 
 	private boolean keepRunning;
 	private double Fps = 60D;
+	long currentTime = System.nanoTime();
+	double nsPerTick = 1000000000D / Fps;
+	long lastTimer = System.currentTimeMillis();
+	double delta = 0;
 	private ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 	private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 
@@ -30,7 +33,28 @@ public class Game implements Serializable {
 
 	public void run() {
 
-		update();
+		while (keepRunning != false) {
+			long present = System.nanoTime();
+			delta += (present - currentTime) / nsPerTick;
+			currentTime = present;
+			while (delta >= 1) {
+
+				delta -= 1;
+				render();
+
+			}
+
+			try {
+				Thread.sleep(3);
+				if (System.currentTimeMillis() - lastTimer >= 1000) {
+					lastTimer += 1000;
+				}
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+			moveSprites();
+			checkForCollisions();
+		}
 
 	}
 
@@ -74,40 +98,9 @@ public class Game implements Serializable {
 
 	}
 
-	public void update() {
-		long currentTime = System.nanoTime();
-		double nsPerTick = 1000000000D / Fps;
-		long lastTimer = System.currentTimeMillis();
-		double delta = 0;
-
-		while (keepRunning != false) {
-			long present = System.nanoTime();
-			delta += (present - currentTime) / nsPerTick;
-			currentTime = present;
-			while (delta >= 1) {
-
-				delta -= 1;
-				render();
-
-			}
-
-			try {
-				Thread.sleep(3);
-				if (System.currentTimeMillis() - lastTimer >= 1000) {
-					lastTimer += 1000;
-				}
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-			moveSprites();
-			checkForCollisions();
-		}
-
-	}
-
 	public void setup() {
 		keepRunning = true;
-		// keyHandler.bindKey(frame);
+		keyHandler.bindKey(frame);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
 		frame.pack();
