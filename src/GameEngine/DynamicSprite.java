38a5b5f13cdd59588f0341e25d2742c10d05fa65
Ellioteff <elliotfaltstrom@hotmail.com>
2015-02-05 21:18:35 +0100

    kommentarer

--- src/GameEngine/DynamicSprite.java
+++ src/GameEngine/DynamicSprite.java
@@ -15,7 +15,7 @@ public class DynamicSprite extends Sprite {
 	protected boolean onGround = false;
 	protected boolean hasHitbox = false;
 	protected ArrayList<Rectangle> hitboxes;
-
+	protected boolean displayHitbox = false;
 
 	public DynamicSprite(int width, int height, int x, int y, BufferedImage image) {
 		super(width, height, x, y, image);
@@ -29,7 +29,7 @@ public class DynamicSprite extends Sprite {
 	}
 
 	protected void stepBackFromCollision() {
-		yPos = (long)previousY-1;
+		yPos = (long) previousY - 1;
 		yVelocity = 0;
 		onGround = true;
 	}
@@ -50,15 +50,18 @@ public class DynamicSprite extends Sprite {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-// the code below can be used to display the hitbox		
-//		if (hasHitbox) {
-//			for (Rectangle r : hitboxes)
-//				g.fillRect(((int) (r.x + xPos)), ((int) (r.y + yPos)), r.width, r.height);
-//		}
+		// the code below can be used to display the hitbox of the sprite by adding the local variables in the hitbox to the global position
+		// in xPos and yPos. set the variable displayHitbox to true to see the hitbox.
+		if (hasHitbox && displayHitbox) {
+			for (Rectangle r : hitboxes)
+				g.fillRect(((int) (r.x + xPos)), ((int) (r.y + yPos)), r.width, r.height);
+		}
 
 	}
 
 	public void addHitbox(int x, int y, int width, int height) {
+		//creates a new arraylist if no hitbox currently exists in the sprite, then creates a rectangle from the paramters
+		//and adds them to the hitbox. Sets the hasHitbox variable to true to make that the hitbox is used in collision detection.
 		if (hitboxes == null)
 			hitboxes = new ArrayList<Rectangle>();
 		hitboxes.add(new Rectangle(x, y, width, height));
@@ -72,13 +75,12 @@ public class DynamicSprite extends Sprite {
 	public boolean isSolid() {
 		return isSolid;
 	}
-	
-	public void setPrevious(){
+	//sets to previous values so that they can be used to step back one step in the loop if a collision with ground occurs.
+	public void setPrevious() {
 		previousX = xPos;
 		previousY = yPos;
 	}
-		
-	
+	//the standard move method that sets the previous values and move the x and y values by using the respective velocities.
 	public void move() {
 		this.setPrevious();
 		xPos += xVelocity;
--- src/GameEngine/Sprite.java
+++ src/GameEngine/Sprite.java
@@ -15,6 +15,8 @@ public abstract class Sprite extends JComponent {
 	protected double yPos;
 	protected int health = 100;
 
+	// returns a copy of the rectangle that represents the spritearea; the area
+	// in which the sprite exists.
 	@Override
 	public Rectangle getBounds() {
 		Rectangle temp = new Rectangle(spriteArea);
@@ -24,19 +26,29 @@ public abstract class Sprite extends JComponent {
 	public int getHealth() {
 		return health;
 	}
+	//constructor that creates a spriteArea out of the variables sent in. Requires a bufferedimage which is why the loadImage method exists
+	//in Loader.java
+	protected Sprite(int width, int height, int x, int y, BufferedImage image) {
+		spriteArea = new Rectangle(x, y, width, height);
+		xPos = x;
+		yPos = y;
+		spriteImage = image;
+	}
 
-
+	// overrides jcomponents paintcomponent which allows for automatic redrawing
+	// when the redraw method is called in the JFrame
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		// sets the spriteareas x and y values to the same as xpos and ypos and
+		// rounds them down to whole integers
 		spriteArea.x = (int) Math.round(xPos);
 		spriteArea.y = (int) Math.round(yPos);
-		int x = (int) Math.round(xPos);
-		int y = (int) Math.round(yPos);
-		g.drawImage(spriteImage, x, y, (int) spriteArea.getWidth(), (int) spriteArea.getHeight(), this);
+		// draws the image on the JFrame which it has been added to.
+		g.drawImage(spriteImage, spriteArea.x, spriteArea.y, (int) spriteArea.getWidth(), (int) spriteArea.getHeight(), this);
 
 	}
-	
+
 	public double getxPos() {
 		return xPos;
 	}
@@ -53,20 +65,9 @@ public abstract class Sprite extends JComponent {
 		this.yPos = yPos;
 	}
 
+	public String toString() {
+		return "X-position: " + xPos + "\nY-Position: " + yPos + "\n";
 
-	protected Sprite(int width, int height, int x, int y, BufferedImage image) {
-		spriteArea = new Rectangle(x, y, width, height);
-		xPos = x;
-		yPos = y;
-		spriteImage = image;
 	}
-	
-	public String toString(){
-		return "X-position: "+xPos+"\nY-Position: "+yPos+"\n";
-		
-	}
-
-
-	
 
 }
